d6cb60be705815017bef62e5e94df0661ada499f
custompro98 <mitchjoa@gmail.com>
2023-12-05 11:00:08 -0500

    2023-12-01-2

--- 2023/1.go
+++ 2023/1.go
@@ -4,58 +4,116 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"regexp"
+	"sort"
 	"strconv"
-	// "strings"
+	"strings"
 )
+
+var targets = []string{
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"one",
+	"two",
+	"three",
+	"four",
+	"five",
+	"six",
+	"seven",
+	"eight",
+	"nine",
+}
+
+type Match struct {
+	startIdx int
+	endIdx   int
+	value    string
+}
 
 func main() {
 	lines, err := readLines(os.Args[1])
-    digits := make([]int, 0)
+	digits := make([]int, 0)
 
-    if err != nil {
-        panic(fmt.Sprintf("failed to parse file: %v", err))
-    }
+	if err != nil {
+		panic(fmt.Sprintf("failed to parse file: %v", err))
+	}
 
-    r, err := regexp.Compile("([0-9])")
+	for idx, line := range lines {
+		matches := make([]Match, 0)
 
-    if err != nil {
-        panic(fmt.Sprintf("failed to make regex: %v", err))
-    }
+		for _, target := range targets {
+			startIdx := strings.Index(line, target)
 
-    for _, line := range lines {
-        m := r.FindAllString(line, -1)
+			if startIdx > -1 {
+				matches = append(matches, Match{
+					startIdx: startIdx,
+					endIdx:   startIdx + len(target),
+					value:    line[startIdx : startIdx+len(target)],
+				})
+			}
 
-        // m[0] = strings.ReplaceAll(m[0], "one", "1")
-        // m[0] = strings.ReplaceAll(m[0], "two", "2")
-        // m[0] = strings.ReplaceAll(m[0], "three", "3")
-        // m[0] = strings.ReplaceAll(m[0], "four", "4")
-        // m[0] = strings.ReplaceAll(m[0], "five", "5")
-        // m[0] = strings.ReplaceAll(m[0], "six", "6")
-        // m[0] = strings.ReplaceAll(m[0], "seven", "7")
-        // m[0] = strings.ReplaceAll(m[0], "eight", "8")
-        // m[0] = strings.ReplaceAll(m[0], "nine", "9")
-        //
-        // m[len(m)-1] = strings.ReplaceAll(m[len(m)-1], "one", "1")
-        // m[len(m)-1] = strings.ReplaceAll(m[len(m)-1], "two", "2")
-        // m[len(m)-1] = strings.ReplaceAll(m[len(m)-1], "three", "3")
-        // m[len(m)-1] = strings.ReplaceAll(m[len(m)-1], "four", "4")
-        // m[len(m)-1] = strings.ReplaceAll(m[len(m)-1], "five", "5")
-        // m[len(m)-1] = strings.ReplaceAll(m[len(m)-1], "six", "6")
-        // m[len(m)-1] = strings.ReplaceAll(m[len(m)-1], "seven", "7")
-        // m[len(m)-1] = strings.ReplaceAll(m[len(m)-1], "eight", "8")
-        // m[len(m)-1] = strings.ReplaceAll(m[len(m)-1], "nine", "9")
+			startIdx = strings.LastIndex(line, target)
 
-        fmt.Println(line, m[0], m[len(m)-1])
+			if startIdx > -1 {
+				matches = append(matches, Match{
+					startIdx: startIdx,
+					endIdx:   startIdx + len(target),
+					value:    line[startIdx : startIdx+len(target)],
+				})
+			}
+		}
 
-        i, err := strconv.Atoi(fmt.Sprintf("%v%v", m[0], m[len(m)-1]))
+		sort.Slice(matches, func(i, j int) bool {
+			return matches[i].startIdx < matches[j].startIdx
+		})
 
-        if err != nil {
-            panic(fmt.Sprintf("failed to convert to int: %v", err))
-        }
+		first := line[matches[0].startIdx:matches[0].endIdx]
+		last := line[matches[len(matches)-1].startIdx:matches[len(matches)-1].endIdx]
 
-        digits = append(digits, i)
-    }
+		if idx == 5 {
+			fmt.Println("---")
+			fmt.Println(line)
+			fmt.Println(matches)
+			fmt.Println(first, last)
+			fmt.Println("---")
+		}
+
+		first = strings.ReplaceAll(first, "one", "1")
+		first = strings.ReplaceAll(first, "two", "2")
+		first = strings.ReplaceAll(first, "three", "3")
+		first = strings.ReplaceAll(first, "four", "4")
+		first = strings.ReplaceAll(first, "five", "5")
+		first = strings.ReplaceAll(first, "six", "6")
+		first = strings.ReplaceAll(first, "seven", "7")
+		first = strings.ReplaceAll(first, "eight", "8")
+		first = strings.ReplaceAll(first, "nine", "9")
+
+		last = strings.ReplaceAll(last, "one", "1")
+		last = strings.ReplaceAll(last, "two", "2")
+		last = strings.ReplaceAll(last, "three", "3")
+		last = strings.ReplaceAll(last, "four", "4")
+		last = strings.ReplaceAll(last, "five", "5")
+		last = strings.ReplaceAll(last, "six", "6")
+		last = strings.ReplaceAll(last, "seven", "7")
+		last = strings.ReplaceAll(last, "eight", "8")
+		last = strings.ReplaceAll(last, "nine", "9")
+
+		// fmt.Println(idx, first, last)
+
+		i, err := strconv.Atoi(fmt.Sprintf("%v%v", first, last))
+
+		if err != nil {
+			panic(fmt.Sprintf("failed to convert to int: %v", err))
+		}
+
+		digits = append(digits, i)
+	}
 
 	sum := 0
 
@@ -67,19 +125,19 @@ func main() {
 }
 
 func readLines(path string) ([]string, error) {
-    out := make([]string, 0)
+	out := make([]string, 0)
 
 	file, err := os.Open(path)
 
 	if err != nil {
-        return out, err
+		return out, err
 	}
 
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
-        out = append(out, scanner.Text())
-    }
+		out = append(out, scanner.Text())
+	}
 
-    return out, nil
+	return out, nil
 }
