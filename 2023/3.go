00f99912c9358e9793a8273cb276154e94a1e064
custompro98 <mitchjoa@gmail.com>
2023-12-08 14:41:48 -0500

    2023-12-03-2

--- 2023/3.go
+++ 2023/3.go
@@ -36,6 +36,12 @@ type Number struct {
 	value  int
 }
 
+type Gear struct {
+	coord Coordinate
+	partOne Number
+	partTwo Number
+}
+
 type Schematic = [][]string
 
 func main() {
@@ -57,7 +63,8 @@ func main() {
 		}
 	}
 
-	numbers := make([]Number, 0)
+	numbers := make([]*Number, 0)
+	partnos := make([]*Number, 0)
 
 	rowno := 0
 	colno := 0
@@ -76,7 +83,7 @@ func main() {
 
 				value, _ := strconv.Atoi(numStr)
 
-				numbers = append(numbers, Number{coords, value})
+				numbers = append(numbers, &Number{coords, value})
 			} else {
 				colno += 1
 			}
@@ -90,11 +97,44 @@ func main() {
 
 	for _, number := range numbers {
 		if number.isPartNumber(sch) {
+			partnos = append(partnos, number)
 			sum += number.value
 		}
 	}
 
 	fmt.Println("part 1:", sum)
+
+	gears := make([]Gear, 0)
+
+	rowno = 0
+	colno = 0
+
+	for rowno < len(sch) {
+		for colno < len(sch[rowno]) {
+			coord := Coordinate{rowno, colno}
+			if coord.isGear(sch, partnos) {
+				adjacencies := coord.partAdjacenices(sch, partnos)
+				gears = append(gears, Gear{
+					coord:   coord,
+					partOne: *adjacencies[0],
+					partTwo: *adjacencies[1],
+				})
+			}
+
+			colno += 1
+		}
+
+		rowno += 1
+		colno = 0
+	}
+
+	sum2 := 0
+
+	for _, gear := range gears {
+		sum2 += gear.partOne.value * gear.partTwo.value
+	}
+
+	fmt.Println("part 2:", sum2)
 }
 
 func isDigit(s string) bool {
@@ -112,6 +152,10 @@ func isDigit(s string) bool {
 
 func isDot(s string) bool {
 	return s == "."
+}
+
+func isStar(s string) bool {
+	return s == "*"
 }
 
 func isSymbol(s string) bool {
@@ -150,6 +194,51 @@ func (c *Coordinate) isAdjacentToSymbol(sch Schematic) bool {
 	for _, d := range directions {
 		newCoord := Coordinate{c.rowno + d.ymod, c.colno + d.xmod}
 		if newCoord.isValid(sch) && isSymbol(sch[newCoord.rowno][newCoord.colno]) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (c *Coordinate) isGear(sch Schematic, partnos []*Number) bool {
+	if (!isStar(sch[c.rowno][c.colno])) {
+		return false
+	}
+
+	parts := c.partAdjacenices(sch, partnos)
+
+	return len(parts) == 2
+}
+
+func (c *Coordinate) partAdjacenices(sch Schematic, partnos []*Number) []*Number {
+	adjacencies := make([]*Number, 0)
+
+	for _, partno := range partnos {
+		for _, coord := range partno.coords {
+			if c.isAdjacent(&coord, sch) && !isPartAlreadyCounted(adjacencies, partno) {
+				adjacencies = append(adjacencies, partno)
+			}
+		}
+	}
+
+	return adjacencies
+}
+
+func isPartAlreadyCounted(partnos []*Number, part *Number) bool {
+	for _, partno := range partnos {
+		if partno == part {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (c *Coordinate) isAdjacent(oc *Coordinate, sch Schematic) bool {
+	for _, d := range directions {
+		nc := Coordinate{oc.rowno + d.ymod, oc.colno + d.xmod}
+		if nc.isValid(sch) && c.rowno == nc.rowno && c.colno == nc.colno {
 			return true
 		}
 	}
